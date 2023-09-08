822b0f21a2d773c0340a7d888f5edb6561e105cf
mossa <cgmossa@gmail.com>
2023-09-08 14:48:03 +0200

    Added "target" awareness

--- notebooks/003_si_model_with_source_and_target.R
+++ notebooks/003_si_model_with_source_and_target.R
@@ -5,6 +5,8 @@ library(magrittr)
 library(tidyverse)
 library(sf)
 
+library(glue)
+
 
 devtools::load_all()
 #'
@@ -21,13 +23,14 @@ devtools::load_all()
 #' @export
 #'
 #' @examples
-create_si_model <- function(grid, beta_mat, y_init) {
+create_si_model <- function(grid, beta_mat, y_init, target_overlap) {
 
   stopifnot(
-    all(c("carry") %in% names(grid))
+    all(c("id", "geometry", "carry") %in% names(grid)),
+    all(dim(beta_mat) == c(length(grid$geometry), length(grid$geometry))),
+    length(y_init) == 2 * length(grid$geometry),
+    all(c("id_overlap", "weight") %in% names(target_overlap))
   )
-
-  # world <- st_union(grid)
 
   parameter_list <- list(
     beta_mat = beta_mat,
@@ -51,12 +54,35 @@ create_si_model <- function(grid, beta_mat, y_init) {
     # reporting
     carry <- parameters$carry
     prevalence_population <- sum(I)  / sum(carry)
+    # prevalence_target <- sum(
+    #   I[target_overlap$id] * target_overlap$weight /
+    #     carry[target_overlap$id] * target_overlap$weight
+    # )
+    prevalence_target <- sum(
+      I[target_overlap$id_overlap] / carry[target_overlap$id_overlap]
+    )
 
     list(c(dS, dI),
+         prevalence_target = prevalence_target,
          prevalence_population = prevalence_population)
   }
 
-  find_target_prevalence <- function(times, y, parameters) {
+  #' @description Terminate when there are no more susceptible,
+  #' `S == 0`, or if there are no more infected,
+  #' `I == 0`.
+  terminate_extinction <- function(times, y, parameters) {
+    N <- parameters$N
+    S <- y[1:N]
+    I <- y[(N + 1):(2 * N)]
+
+    c(
+      zapsmall(sum(S)),
+      zapsmall(sum(I))
+    )
+  }
+
+  #' @description Root-function for population prevalence equal to 50%.
+  find_population_prevalence <- function(times, y, parameters) {
     N <- parameters$N
     I <- y[(N + 1):(2 * N)]
 
@@ -64,27 +90,51 @@ create_si_model <- function(grid, beta_mat, y_init) {
     # could also be divided by sum(y)
     prevalence_population <- sum(I) / sum(carry)
 
-    c(prevalence_population - 0.5, zapsmall(sum(I)))
+    c(prevalence_population - 0.5,
+      terminate_extinction(times, y, parameters))
   }
-  # browser()
+
+  find_target_prevalence <- function(times, y, parameters) {
+    N <- parameters$N
+    I <- y[(N + 1):(2 * N)]
+
+    carry <- parameters$carry
+
+    # prevalence_target <- sum(
+    #   I[target_overlap$id] * target_overlap$weight /
+    #     carry[target_overlap$id] * target_overlap$weight
+    # )
+    prevalence_target <- sum(
+      I[target_overlap$id_overlap] / carry[target_overlap$id_overlap]
+    )
+
+    c(prevalence_target - 0.5,
+      terminate_extinction(times, y, parameters))
+  }
+
+  # common parameters
   ode_parameters <- list(
     verbose = TRUE,
     y = y_init,
     parms = parameter_list,
-    # times = c(0, Inf),
-    # times = seq.default(0, 10, length.out = 250),
-    rootfunc = find_target_prevalence,
     func = model_func,
     ynames = FALSE
   )
   tau_model_output <-
-    rlang::exec(deSolve::ode, !!!ode_parameters, times = c(0, Inf))
+    rlang::exec(deSolve::ode, !!!ode_parameters,
+                rootfunc = find_target_prevalence,
+                times = c(0, Inf))
   #TODO: check if tau exists
   tau <- tau_model_output[2,1]
 
+  #
+
+  #' this follows the population from 0 until
+  #' tau (time when prevalence(target) = 50%)
   model_output <-
     rlang::exec(deSolve::ode, !!!ode_parameters,
-                times = seq.default(0, tau, length.out = 250))
+                times = seq.default(0, tau, length.out = 250),
+                rootfunc = terminate_extinction)
 
   list(
     tau_model_output = tau_model_output,
@@ -110,41 +160,85 @@ ggplot() +
           aes(fill = label, geometry = buffer_polygon)) +
   theme_blank_background()
 
-
 world_area <- st_area(world_landscape)
 
 # n <- 100
 # n <- 50
 n <- 30
 grid <- create_grid(n, world_landscape, landscape_scale = world_scale)
-
+grid <- grid %>% rowid_to_column("id")
 population_total <- world_area
-
 grid$carry <- st_area(grid$geometry)
 
 y_init <- c(S = grid$carry,
             I = numeric(length(grid$carry)))
-# find closest to center
-closest_to_middle <-
-  st_nearest_feature(world_landscape %>% st_centroid(),
-                     grid$geometry %>% st_centroid())
-ggplot() +
-  geom_sf(data = grid, fill = NA) +
+#'
+#' Define source- and target-buffer indices and weights
+source_target_overlap <-
+  # this assumes that `grid` has `id` in it
+  st_intersection(source_target %>%
+                    `st_geometry<-`("buffer_polygon"),
+                  grid) %>%
+  rename(id_overlap = id,
+         buffer_overlap = buffer_polygon) %>%
+  mutate(overlap_area = st_area(buffer_overlap),
+         # normalise with corresponding grid cells' area
+         weight = overlap_area / st_area(grid$geometry[id_overlap]),
+         overlap_area = NULL) %>%
+  identity()
+#'
+source_target_overlap
 
-  geom_sf(data = grid[closest_to_middle,],
-          fill = "blue") +
+
+
+ggplot() +
+  geom_sf(data = source_target_overlap, fill = NA) +
   theme_blank_background()
+#'
+#'
+#'
+
+# reduce the carried over information
+source_target_overlap <- source_target_overlap %>%
+  as_tibble() %>%
+  select(-carry, -buffer_area, -buffer_point, -buffer_overlap) %>%
+  identity()
+# Ignore this warning
+
+source_target_overlap_list <-
+  source_target_overlap %>%
+  nest(data = -label) %>%
+  deframe()
+
+source_target_overlap
+
+source_overlap <- source_target_overlap_list$source
+target_overlap <- source_target_overlap_list$target
+
+source_overlap
+target_overlap
+
 
 #' Add infected animal
-y_init[nrow(grid) + closest_to_middle] <- 0.5 * grid$carry[closest_to_middle]
-y_init[closest_to_middle] <- y_init[nrow(grid) + closest_to_middle]
 
-y_init
+half_infected_mass <-
+  grid$carry[source_overlap$id_overlap] *
+  source_overlap$weight *
+  (1/2)
+#' remove mass from susceptible
+y_init[source_overlap$id_overlap] <-
+  y_init[source_overlap$id_overlap] - half_infected_mass
+y_init[
+  nrow(grid) +
+    source_overlap$id_overlap
+] <- +half_infected_mass
 
-# ignoring
-y_init %>%
-  zapsmall() %>%
-  table()
+stopifnot(
+  "compartments must be non-negative" =
+    all(y_init >= 0),
+  "equal mass to preset carrying capacity" =
+    isTRUE(all.equal(sum(y_init), sum(grid$carry)))
+)
 
 beta_baseline <- 0.05
 dist_grid <- st_distance(grid$geometry %>% st_centroid())
@@ -183,31 +277,50 @@ dist_grid
 #                         order(dist_grid[closest_to_middle,])]
 # Matrix::Matrix(dist_grid3) %>% Matrix::image()
 
-
-y_init[(1:nrow(grid))] -> S
-y_init[(nrow(grid) + 1):(2 * nrow(grid))] -> I
-
-sum(I) / sum(S + I)
-sum(I) / sum(grid$carry)
-
-y_init[(1:nrow(grid))] %>% zapsmall() %>% table()
-y_init[(nrow(grid) + 1):(2 * nrow(grid))] %>% table()
-
-create_si_model(grid, beta_mat, y_init) ->
+create_si_model(grid, beta_mat, y_init, target_overlap) ->
   model_output
-model_output$tau_model_output[2,1]
-model_output$model_output %>% str()
-#' Population prevalence throughout
+
+model_output$tau_model_output
+model_output$tau_model_output[2,1] # tau
+
+model_output$model_output
+
+#' Extract population and target prevalence throughout the trajectory:
 model_output$model_output[,c(1, (2*nrow(grid) + 2):ncol(model_output$model_output)),
                           drop = FALSE] ->
   prevalence_throughout
+prevalence_throughout %>% head()
 
 #' Population prevalence throughout
 prevalence_throughout %>%
   as_tibble() %>%
+  pivot_longer(starts_with("prevalence_"),
+               names_to = c(NA, "prevlance_name"),
+               names_sep = "_") %>%
   ggplot() +
-  aes(time, prevalence_population) +
-  geom_line()
+  aes(time, value, group = prevlance_name) +
+  geom_line(aes(color = prevlance_name)) +
+  lims(y = c(0, 0.5)) +
+  theme_blank_background() +
+  theme(legend.position = "bottom") +
+  labs(caption = "{n}")
+
+I_grid <-
+  model_output$tau_model_output[2, (nrow(grid) + 2):(2 * nrow(grid) + 1)] %>%
+  # names()
+  identity()
+prevalence_grid <- (I_grid / grid$carry)
+ggplot() +
+  geom_sf(data = grid %>% mutate(prev = prevalence_grid),
+          aes(fill = prev)) +
+
+  geom_sf(data = source_target,
+          fill = NA,
+          aes(color = label, geometry = buffer_polygon)) +
+
+  scale_fill_viridis_c() +
+  theme(legend.position = "bottom") +
+  theme_blank_background()
 
 # # model_output[2, (1:nrow(grid))] -> S
 # #
