f27550bf9998e08fcaf305e4a745d967c76c9f1c
mossa <cgmossa@gmail.com>
2023-09-15 08:08:44 +0200

    these comments are wrong

--- notebooks/026_inspecting_order_of_cells_in_st_make_grid.R
+++ notebooks/026_inspecting_order_of_cells_in_st_make_grid.R
@@ -10,21 +10,43 @@ library(sf)
 devtools::load_all()
 
 
-world <- create_landscape(29)
+world <- create_landscape(4)
 world$landscape -> world_landscape
 
-grid <- st_make_grid(world_landscape, cellsize = 29 / 5) %>%
+grid <- st_make_grid(world_landscape, cellsize = c(1, 2)) %>%
   st_sf()
 grid <- rowid_to_column(grid, "id")
-
-# grid_yflip <- grid %>%
-#   mutate(geometry = geometry - )
 
 ggplot() +
   geom_sf(data = grid, fill = NA) +
 
   geom_sf_text(data = grid, aes(label = id)) +
+  labs(caption = "row-major") +
 
+  theme_blank_background()
+
+# NOTE: must fill this out from above.
+grid_nrow <- 2
+grid_ncol <- 4
+
+# convert 1:(nrow×ncol) from row-major to col-major.
+
+grid2 <- grid %>%
+  # id ~> 1:(grid_nrow×grid_ncol)
+  # (id - 1) ~> 0:(grid_nrow×grid_ncol-1)
+  # (id - 1) %% grid_nrow ~> 0:(grid_nrow×grid_ncol - 2)
+  #  -||- × grid_nrow ~> what column is `id`'th element in
+  #
+  # (id - 1) %/% grid_nrow ~> what row is this `id`'th element in
+  slice(((id - 1) %% grid_nrow) * grid_ncol + (id - 1) %/% grid_nrow + 1) %>%
+  mutate(id = NULL) %>%
+  rowid_to_column("id")
+
+
+ggplot() +
+  geom_sf(data = grid, fill = NA) +
+  geom_sf_text(data = grid2, aes(label = id)) +
+  labs(caption = "col-major") +
   theme_blank_background()
 #' ## Conclusion
 #'
