c5250f37c1d8cfe986628bf325eedf66613a4435
mossa <cgmossa@gmail.com>
2023-09-15 06:53:08 +0200

    formatting

--- notebooks/024_area_based_grids.R
+++ notebooks/024_area_based_grids.R
@@ -1,6 +1,6 @@
-#'---
-#'author: Matt
-#'---
+#' ---
+#' author: Matt
+#' ---
 #'
 
 world_landscape
@@ -9,18 +9,26 @@ type <- "hexagons"
 
 grid <-
   if (type == "square") {
-    st_make_grid(world_landscape, cellsize = rep(sqrt(area),2L),
-                 square = TRUE, what = "polygons")
+    st_make_grid(world_landscape,
+      cellsize = rep(sqrt(area), 2L),
+      square = TRUE, what = "polygons"
+    )
   } else if (type == "hexagons") {
     # missing cellsize/sqrt(3) = s derivation, see `create_grid`
-    st_make_grid(world_landscape, cellsize = sqrt((2 * area) / 3 * sqrt(3)),
-                 square = FALSE, what = "polygons")
+    st_make_grid(world_landscape,
+      cellsize = sqrt((2 * area) / 3 * sqrt(3)),
+      square = FALSE, what = "polygons"
+    )
   } else if (type == "hexagons_rot") {
-    st_make_grid(world_landscape, cellsize = sqrt((2 * area) / 3 * sqrt(3)),
-                 square = FALSE, flat_topped = TRUE, what = "polygons")
+    st_make_grid(world_landscape,
+      cellsize = sqrt((2 * area) / 3 * sqrt(3)),
+      square = FALSE, flat_topped = TRUE, what = "polygons"
+    )
   } else if (type == "triangles") {
-    st_make_grid(world_landscape, cellsize = rep(sqrt(area * 2L), 2L),
-                 square = TRUE, what = "polygons") %>%
+    st_make_grid(world_landscape,
+      cellsize = rep(sqrt(area * 2L), 2L),
+      square = TRUE, what = "polygons"
+    ) %>%
       st_triangulate_constrained() %>%
       st_collection_extract()
   }
@@ -31,13 +39,19 @@ grid %<>%
   dplyr::filter(st_area(geometry) > 0)
 
 
-grid %>% ggplot() + geom_sf()
+grid %>% ggplot() +
+  geom_sf()
 
 
 grid %>% st_area()
-grid %>% st_area() %>% sum()
+grid %>%
+  st_area() %>%
+  sum()
 st_area(world_landscape)
-grid %>% st_area() %>% density() %>% plot()
+grid %>%
+  st_area() %>%
+  density() %>%
+  plot()
 
 #
 # sf:::make_hex_grid()
