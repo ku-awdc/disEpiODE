b00a4c1c1d4c523709435a956b1644eeab639dab
mossa <cgmossa@gmail.com>
2023-09-15 23:54:14 +0200

    draft

---
+++ R/seq_cellarea.R
@@ -0,0 +1,22 @@
+#' Generate `cellarea` sequence
+#'
+#' Geometric sequence of `cellarea` used in [create_grid] based
+#' on increasing precision
+#'
+#' @param precision Numeric between `0` and `1`
+#' @param min_cellarea,max_cellarea
+#'
+#' @return Sequence between `min_cellarea` and `max_cellarea`
+#' @export
+#'
+#' @examples
+seq_cellarea <- function(precision, min_cellarea, max_cellarea) {
+  stopifnot(
+
+    0 <= precision && precision <= 1,
+    min_cellarea <= max_cellarea
+  )
+  max_x <- log(min_cellarea / max_cellarea) / log(1 - precision)
+  max_x <- floor(max_x)
+  max_cellarea * (1 - precision) ** (0:max_x)
+}
