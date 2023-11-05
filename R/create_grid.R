cf39481e94f3c83b146935238cdcfed817f536c1
cgmossa <cgmossa@gmail.com>
2023-11-05 18:09:09 +0100

    added middle for square only

--- R/create_grid.R
+++ R/create_grid.R
@@ -4,6 +4,8 @@
 #' @param cellarea Positive number that is less than or equal to `st_area(landscape)`.
 #' @param celltype Cell shapes `c("square", "hexagon", "hexagon_rot", "triangle")`
 #' @param offset Aligned with initial cell placed at lower left corner
+#' @param n Integer specifying how many cells to place horizontally and vertically.
+#' @param middle Offset the grid so equal sized cells are in the middle.
 #'
 #' @note In `offset`, `"bottom"` means aligned to bottom;
 #' Thus bottom middle.
@@ -21,6 +23,7 @@
 create_grid <- function(landscape,
                         cellarea = NULL,
                         n = NULL,
+                        middle = FALSE,
                         celltype = c("square", "hexagon", "hexagon_rot", "triangle"),
                         offset = c("corner", "middle", "bottom", "left")) {
   stopifnot(
@@ -30,8 +33,9 @@ create_grid <- function(landscape,
   )
 
   if (!is.null(cellarea)) {
+    # provided cellarea
     stopifnot(
-      "`cellarea must be postive number" = cellarea > 0,
+      "`cellarea must be positive number" = cellarea > 0,
       "`cellarea` must not exceed provided `landscape` in size" =
         cellarea <= sum(st_area(landscape)),
       "`cellarea` must be of size 1 or 2" =
@@ -40,6 +44,7 @@ create_grid <- function(landscape,
   } else {
     # !is.null(n)
     # ???
+    # Warning: if middle is specified, than don't do anything?
   }
 
   celltype <- match.arg(celltype, celltype)
@@ -62,21 +67,41 @@ create_grid <- function(landscape,
   #   )
 
   # FIXME: `offset` is missing here! it is not enough just to uncomment the above
+  landscape_bbox <- st_bbox(landscape)
+  landscape_bbox_dim <- landscape_bbox[c(3, 4)] -
+    landscape_bbox[c(1, 2)]
   grid <-
-    switch(
-      celltype,
+    switch(celltype,
       square = {
         if (!is.null(cellarea)) {
-          cellsize <- sqrt(c(cellarea, cellarea))
-          st_make_grid(landscape, cellsize = cellsize,
-                       square = TRUE, what = "polygons")
-        } else {
-          #!is.null(n)
-          st_make_grid(landscape,
-                       n = n,
-                       square = TRUE, what = "polygons")
-        }
+          # cellarea provided
+          if (middle) {
+            # offset to the middle
+            cellsize <- sqrt(c(cellarea, cellarea))
 
+            offset <- landscape_bbox_dim %% cellsize
+            offset <- cellsize - offset / 2
+
+            st_make_grid(landscape,
+              cellsize = cellsize,
+              offset = -offset,
+              square = TRUE, what = "polygons"
+            )
+          } else {
+            # don't offset to the "middle"
+            cellsize <- sqrt(c(cellarea, cellarea))
+            st_make_grid(landscape,
+              cellsize = cellsize,
+              square = TRUE, what = "polygons"
+            )
+          }
+        } else {
+          # !is.null(n)
+          st_make_grid(landscape,
+            n = n,
+            square = TRUE, what = "polygons"
+          )
+        }
       },
       hexagon = {
         # Solve side-length for hexagons area
@@ -96,55 +121,63 @@ create_grid <- function(landscape,
         if (!is.null(cellarea)) {
           cellsize <- sqrt((2 * cellarea) / sqrt(3))
           # NOTE: see `sf:::make_hex_grid` for more details
-          st_make_grid(landscape, cellsize = cellsize,
-                       square = FALSE, what = "polygons")
+          st_make_grid(landscape,
+            cellsize = cellsize,
+            square = FALSE, what = "polygons"
+          )
         } else {
           # !is.null(n)
           st_make_grid(landscape,
-                       n = n,
-                       square = FALSE, what = "polygons")
+            n = n,
+            square = FALSE,
+            what = "polygons"
+          )
         }
-
       },
       hexagon_rot = {
         if (!is.null(cellarea)) {
           cellsize <- sqrt((2 * cellarea) / 3 * sqrt(3))
-          st_make_grid(landscape, cellsize = cellsize,
-                       square = FALSE, flat_topped = TRUE, what = "polygons")
+          st_make_grid(landscape,
+            cellsize = cellsize,
+            square = FALSE, flat_topped = TRUE, what = "polygons"
+          )
         } else {
           # !is.null(n)
-          st_make_grid(landscape, n = n, square = FALSE,
-                       flat_topped = TRUE, what = "polygons")
+          st_make_grid(landscape,
+            n = n, square = FALSE,
+            flat_topped = TRUE, what = "polygons"
+          )
         }
       },
       triangle = {
         if (!is.null(cellarea)) {
           # cellsize <- rep(sqrt(cellarea * 2L), 2L)
           cellsize <- sqrt(c(2 * cellarea, 2 * cellarea))
-          st_make_grid(landscape, cellsize = cellsize,
-                       square = TRUE, what = "polygons") %>%
+          st_make_grid(landscape,
+            cellsize = cellsize,
+            square = TRUE, what = "polygons"
+          ) %>%
             st_triangulate_constrained() %>%
             st_collection_extract()
-
         } else {
           # !is.null(n)
           st_make_grid(landscape,
-                       n = pmax(1, round(n / 2)),
-                       square = TRUE,
-                       what = "polygons") %>%
+            n = pmax(1, round(n / 2)),
+            square = TRUE,
+            what = "polygons"
+          ) %>%
             st_triangulate_constrained() %>%
             st_collection_extract()
         }
-
       }
     )
 
-  # grid <- grid %>%
-  #   st_intersection(landscape, dimensions = "polygon") %>%
-  #   st_sf() %>%
-  #   # note: for hexagon, sometimes we have LINESTRING here..
-  #   # dplyr::filter(st_area(geometry) > 0)
-  #   identity()
+  grid <- grid %>%
+    st_intersection(landscape, dimensions = "polygon") %>%
+    st_sf() %>%
+    # note: for hexagon, sometimes we have LINESTRING here..
+    # dplyr::filter(st_area(geometry) > 0)
+    identity()
 
   matched_area <- isTRUE(all.equal(
     st_area(st_union(grid)),
@@ -156,8 +189,9 @@ create_grid <- function(landscape,
     # fix an issue with `st_make_grid` where the boundary gets an extra
     # set of grids sometimes..
     grid <- st_filter(grid,
-                      landscape,
-                      .predicate = st_within)
+      landscape,
+      .predicate = st_within
+    )
   }
 
   # Amend grid with properties useful elsewhere
@@ -192,7 +226,9 @@ create_grid <- function(landscape,
     "grid area must equal landscape area" =
       isTRUE(all.equal(
         st_area(st_union(grid)),
-        st_area(landscape))))
+        st_area(landscape)
+      ))
+  )
 
   grid
 }
---
+++ tests/testthat/test-grid_middle.R
@@ -0,0 +1,82 @@
+test_that("middle grid resolution", {
+
+
+  world_scale <- 5.8 * 10
+  # world_scale <- 22.5**2
+  world_landscape <- st_bbox(c(xmin = 0, xmax = world_scale,
+                               ymin = 0, ymax = world_scale)) %>%
+    st_as_sfc()
+
+  cellarea <- 10**2
+  {
+    grid <- create_grid(landscape = world_landscape,
+                        cellarea = cellarea) %>%
+      mutate(area_flag = area %>% zapsmall() %>% factor())
+
+    grid_middle <- create_grid(landscape = world_landscape,
+                               cellarea = cellarea,
+                               middle = TRUE) %>%
+      mutate(area_flag = area %>% zapsmall() %>% factor())
+    grids <- bind_rows(
+      middle = grid_middle,
+      regular = grid,
+      .id = "mode"
+    )
+
+    ggplot() +
+      geom_sf(data = grids, aes(fill = area_flag, group = mode)) +
+      coord_sf(expand = FALSE) +
+      facet_wrap(~mode) +
+      theme_blank_background()
+  }
+
+
+  # ggplot() +
+  #
+  #   # geom_sf(fill = NA,
+  #   #         data = grid) +
+  #   geom_sf(data = grid, aes(fill = area_flag)) +
+  #   coord_sf(expand = FALSE) +
+  #   theme_blank_background()
+  # ggplot() +
+  #
+  #   # geom_sf(fill = NA,
+  #   #         data = grid) +
+  #   geom_sf(data = grid_middel, aes(fill = area_flag)) +
+  #   coord_sf(expand = FALSE) +
+  #   theme_blank_background()
+
+})
+
+
+test_that("square middle grid resolution", {
+
+
+  # world_scale <- 39
+  world_scale <- 39
+  world_landscape <- st_bbox(c(xmin = 0, xmax = world_scale,
+                               ymin = 0, ymax = world_scale)) %>%
+    st_as_sfc()
+
+  for (cellarea in seq_cellarea(n = 100,
+                                min_cellarea = 5,
+                                max_cellarea = world_scale**2) %>%
+       rev()) {
+
+    grid_middle <- create_grid(landscape = world_landscape,
+                               cellarea = cellarea,
+                               middle = TRUE)
+    ggplot(grid_middle) +
+
+      geom_sf(aes(fill =  area %>% zapsmall() %>% factor()),
+              data = grid_middle) +
+      coord_sf(expand = FALSE) +
+      theme_blank_background()
+
+    # inner cells, corner, and edges
+    expect_lte(
+      grid_middle$area %>% zapsmall() %>% n_distinct(),
+      3
+    )
+  }
+})
