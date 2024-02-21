9670d95e9fe362122a848689991af4effaac2b67
Mossa <cgmossa@gmail.com>
2024-02-21 10:13:48 +0100

    added a dedicated script for one of the plots in the paper

---
+++ notebooks/075_grid_generation_plot_diagnostic.R
@@ -0,0 +1,66 @@
+world_scale <- 1
+
+cellarea <- seq_cellarea(precision = 0.05, min_cellarea = 1 / 2000, max_cellarea = world_scale**2)
+
+generate_grids <- tibble(cellarea) %>%
+  expand_grid(celltype = c("triangle", "square", "hexagon")) %>%
+  # DEBUG
+  # slice(601) %>%
+  # glimpse() %>%
+
+  mutate(grid =
+           map2(celltype,  cellarea, \(celltype, cellarea)
+                create_grid(
+                  world_landscape,
+                  cellarea = cellarea,
+                  celltype = celltype
+                )))
+generate_grids <-  generate_grids %>%
+  mutate(n = grid %>% map_dbl(nrow),
+         n_unique_area = grid %>% map_dbl(\(grid) {
+           st_area(grid) %>% zapsmall() %>% n_distinct()
+         })) %>%
+  #' validation purposes
+  mutate(mean_actual_cellarea = grid %>% map_dbl(\(grid) mean(st_area(grid))),
+         median_actual_cellarea = grid %>% map_dbl(\(grid) median(st_area(grid)))
+  )
+
+p_ncells_plot <- generate_grids %>%
+  ggplot() +
+  aes(cellarea, n) +
+  # geom_line(aes(color = celltype)) +
+  geom_step(aes(color = celltype)) +
+
+  # stat_function(fun = ~ -.x) +
+  # geom_abline(slope = log10(0), slope = 1) +
+  # geom_abline(aes(intercept = 0, slope = 1,
+  #                 y = stage(after_scale = y))) +
+
+  # scale_x_log10_rev() +
+  # theme_reverse_arrow_x() +
+  # scale_y_log10() +
+
+  theme(legend.position = "bottom") +
+  labs(color = NULL,
+       x = "Cell area",
+       y = "Total number of cells") +
+  theme_blank_background()
+p_ncells_plot
+p_ncells_plot + aes(y = mean_actual_cellarea) + labs(y = "Avg. cell areas in grid") +
+
+  # geom_abline(intercept = 0, slope = -1, linetype = "dotted") +
+  # scale_x_reverse() +
+
+  # stat_function(fun = function(x) x, linetype = "dotted") +
+  # scale_x_reverse() +
+
+  stat_function(fun = function(x) x, linetype = "dotted") +
+  scale_x_log10_rev() +
+  theme_reverse_arrow_x() +
+
+  # identity
+  # stat_function(fun = function(x) x, linetype = "dotted") +
+
+  # coord_equal() +
+  # expand_limits(y = 1) +
+  NULL
