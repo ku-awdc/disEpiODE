81360865912828bcd83ebdbfc3a86f9da87f5b3e
Mossa <cgmossa@gmail.com>
2024-02-21 14:12:07 +0100

    retooled the plot but this doesn't help much

--- notebooks/075_grid_generation_plot_diagnostic.R
+++ notebooks/075_grid_generation_plot_diagnostic.R
@@ -1,15 +1,22 @@
-world_scale <- 1
+devtools::load_all(".")
 
-cellarea <- seq_cellarea(precision = 0.05, min_cellarea = 1 / 2000, max_cellarea = world_scale**2)
+world <- create_landscape(world_scale <- 1)
+world$landscape -> world_landscape
+
+cellarea <- seq_cellarea(precision = 0.01,
+                         min_cellarea = 1 / 2000,
+                         max_cellarea = world_scale**2)
+cellarea
+#' Remove cellareas that correspond to already seen cellareas.
+# 1 / cellarea[which(!duplicated(zapsmall(1 / cellarea)))]
+# cellarea <- cellarea[which(!duplicated(round(1 / cellarea)))]
 
 generate_grids <- tibble(cellarea) %>%
   expand_grid(celltype = c("triangle", "square", "hexagon")) %>%
-  # DEBUG
-  # slice(601) %>%
-  # glimpse() %>%
 
   mutate(grid =
-           map2(celltype,  cellarea, \(celltype, cellarea)
+           map2(.progress = TRUE,
+                celltype,  cellarea, \(celltype, cellarea)
                 create_grid(
                   world_landscape,
                   cellarea = cellarea,
@@ -28,8 +35,11 @@ generate_grids <-  generate_grids %>%
 p_ncells_plot <- generate_grids %>%
   ggplot() +
   aes(cellarea, n) +
-  # geom_line(aes(color = celltype)) +
   geom_step(aes(color = celltype)) +
+  # geom_point(aes(color = celltype)) +
+  # geom_step(aes(color = celltype), position = position_jitter(height = 0.01)) +
+  # geom_step(aes(color = celltype), position = position_jitter(height = 0.01)) +
+  # geom_line(aes(color = celltype)) +
 
   # stat_function(fun = ~ -.x) +
   # geom_abline(slope = log10(0), slope = 1) +
@@ -45,7 +55,6 @@ p_ncells_plot <- generate_grids %>%
        x = "Cell area",
        y = "Total number of cells") +
   theme_blank_background()
-p_ncells_plot
 p_ncells_plot + aes(y = mean_actual_cellarea) + labs(y = "Avg. cell areas in grid") +
 
   # geom_abline(intercept = 0, slope = -1, linetype = "dotted") +
@@ -63,4 +72,19 @@ p_ncells_plot + aes(y = mean_actual_cellarea) + labs(y = "Avg. cell areas in gri
 
   # coord_equal() +
   # expand_limits(y = 1) +
+  geom_rug(sides = "b", length = unit(0.03 / 3, "npc")) +
+  # geom_rug(sides = "b", length = unit(0.03 / 3, "npc"), aes(color = celltype),
+  #          position = position_dodge(width = 0.5, preserve = "total")
+  #          ) +
   NULL
+p_ncells_plot
+#'
+#'
+# DEBUG
+# ggplot() +
+#   geom_sf(data =
+#             generate_grids$grid[[424]], fill = NA)
+
+#'
+beepr::beep()
+
