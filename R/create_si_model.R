c9bf9f6362246a11aad3b0c6e377b403f0501b12
Mossa <cgmossa@gmail.com>
2024-02-24 15:30:16 +0100

    model_func: MAJOR CHANGE Now returns prevalences for source, middle, target and population.

--- R/create_si_model.R
+++ R/create_si_model.R
@@ -1,5 +1,3 @@
-
-
 #' Deterministic SI model with pair-wise infectious interactions
 #'
 #' @param grid Represents a grid of a landscape
@@ -71,6 +69,7 @@ create_si_model <- function(grid, beta_mat, y_init, target_overlap, middle_overl
 model_func <- function(times, y, parameters) {
   N <- parameters$N
   beta_mat <- parameters$beta_mat
+  source_overlap <- parameters$source_overlap
   target_overlap <- parameters$target_overlap
   middle_overlap <- parameters$middle_overlap
   area <- parameters$area
@@ -91,16 +90,17 @@ model_func <- function(times, y, parameters) {
   carry <- parameters$carry
   prevalence_population <- sum(I)  / sum(carry) # alternative: divide by sum(S + I) instead
   # weighted arithmetic mean of prevalence
-  prevalence_target <- sum((I[target_overlap$id_overlap] /
-                              carry[target_overlap$id_overlap]) * target_overlap$weight)
-    # sum(target_overlap$weight)
+  prevalence_source <- sum((I[source_overlap$id_overlap] /
+                              carry[source_overlap$id_overlap]) * source_overlap$weight)
   prevalence_middle <- sum((I[middle_overlap$id_overlap] /
                               carry[middle_overlap$id_overlap]) * middle_overlap$weight)
-    # sum(middle_overlap$weight)
+  prevalence_target <- sum((I[target_overlap$id_overlap] /
+                              carry[target_overlap$id_overlap]) * target_overlap$weight)
 
   list(c(dS, dI),
-       prevalence_target = prevalence_target,
+       prevalence_source = prevalence_source,
        prevalence_middle = prevalence_middle,
+       prevalence_target = prevalence_target,
        prevalence_population = prevalence_population)
 }
 
@@ -167,9 +167,11 @@ find_target_prevalence <- function(times, y, parameters) {
 
 #' @description Root-function for terminating the simulation
 #' when the middle-buffer has a prevalence of 50%.
+#' Or terminates when there are no longer any any susceptibles,
+#' or any infected, see [terminate_extinction].
 find_middle_prevalence <- function(times, y, parameters) {
   N <- parameters$N
-  
+
   # NOTE: changing the stopping criteria from TARGET to MIDDLE
   middle_overlap <- parameters$middle_overlap
   I <- y[(N + 1):(2 * N)]
