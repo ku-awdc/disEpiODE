eb02ccc51a845c1f740d92f3cfb9daee079c5e23
Mossa <cgmossa@gmail.com>
2024-03-13 11:36:59 +0100

    formatting and reuse variables

--- notebooks/079_center_as_centroid_concept.R
+++ notebooks/079_center_as_centroid_concept.R
@@ -3,7 +3,6 @@ devtools::load_all()
 landscape <- create_landscape(world_scale <- 1)$landscape
 
 create_grid_with_center <- function(landscape, cellsize) {
-
   landscape_centroid <- st_centroid(landscape)
   centroid_coords <- st_coordinates(landscape_centroid)
   stopifnot(any(length(cellsize) %in% c(1, 2)))
@@ -14,24 +13,24 @@ create_grid_with_center <- function(landscape, cellsize) {
 
 
   dist_center <-
-    landscape %>%
-    st_centroid() %>%
-    st_distance(st_point(c(0,0))) %>%
-    `[`(1,1)
+    landscape_centroid
+  st_distance(st_point(c(0, 0))) %>%
+    `[`(1, 1)
   cellsize <- sqrt(c(cellarea, cellarea))
   celldiagonal <- sqrt(sum(cellsize**2))
   offset <- dist_center %% celldiagonal
   offset_sign <- offset >= celldiagonal / 2
   offset <- -offset + celldiagonal / 2
-  offset <- c(1,1) * offset / sqrt(2)
-  offset <- offset_sign * cellsize  + offset
+  offset <- c(1, 1) * offset / sqrt(2)
+  offset <- offset_sign * cellsize + offset
   offset <- -offset
 
   grid <- st_make_grid(landscape,
-                       cellsize = cellsize,
-                       square = TRUE,
-                       what = "polygons",
-                       offset = offset)
+    cellsize = cellsize,
+    square = TRUE,
+    what = "polygons",
+    offset = offset
+  )
   # grid
   grid %>%
     st_intersection(landscape)
@@ -40,11 +39,14 @@ create_grid_with_center <- function(landscape, cellsize) {
 #'
 ggplot() +
   geom_sf(data = landscape, linetype = "dashed", fill = NA) +
-  geom_sf(data = create_grid_with_center(landscape = landscape, cellsize = 1/6),
-          fill = NA, size = 2) +
-  geom_sf(data = create_grid_with_center(landscape = landscape, cellsize = 1/9),
-          fill = NA, size = 2) +
-
+  geom_sf(
+    data = create_grid_with_center(landscape = landscape, cellsize = 1 / 6),
+    fill = NA, size = 2
+  ) +
+  geom_sf(
+    data = create_grid_with_center(landscape = landscape, cellsize = 1 / 9),
+    fill = NA, size = 2
+  ) +
   geom_sf(data = st_point(c(0.5, 0.5)), aes(color = "center")) +
 
   # theme_grid_plot() +
